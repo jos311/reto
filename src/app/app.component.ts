fd97c00803d8f8ac342b3bb382c3668da52b0f0d
jos311 <jose_ramirezmoran@student.uml.edu>
2024-02-15 16:20:20 -0500

    feat: integrar el wallet adapter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HdWalletMultiButtonComponent } from '@heavy-duty/wallet-adapter-material';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, HdWalletMultiButtonComponent],
   selector: 'hdb-week2-root',
   template: `
-    <header>
-      <h1>Hola, soy tu wallet!</h1>
+    <header class="py-8">
+      <h1 class="text-5xl text-center mb-4">Hola, soy tu wallet!</h1>
+      <div class="flex justify-center">
+      <hd-wallet-multi-button></hd-wallet-multi-button>
+      </div>
     </header>
   `
 })
