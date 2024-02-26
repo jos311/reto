b36ee8fbb15a54876b5e4f1ef9d8dede15fcc24e
jos311 <jose_ramirezmoran@student.uml.edu>
2024-02-26 10:53:30 -0500

    multiplicando la cantidad para pasar el monto correcto

--- src/app/transfer-modal.component.ts
+++ src/app/transfer-modal.component.ts
@@ -1,7 +1,12 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit, inject, computed } from '@angular/core';
 import { TransferFormComponent, TransferFormPayload } from './transfer-form.component';
 import { injectTransactionSender } from '@heavy-duty/wallet-adapter';
 import { createTransferInstructions } from '@heavy-duty/spl-utils';
+import { MatProgressSpinner } from '@angular/material/progress-spinner'
+import { MatDialogRef } from '@angular/material/dialog';
+import {MatSnackBar} from '@angular/material/snack-bar'
+
+
 
 
 @Component({
@@ -14,19 +19,28 @@ import { createTransferInstructions } from '@heavy-duty/spl-utils';
   </div>
   `,
   standalone: true,
-  imports: [TransferFormComponent]
+  imports: [TransferFormComponent, MatProgressSpinner]
 })
 
 export class TransferModalComponent {
   private readonly _transactionSender = injectTransactionSender();
+  private readonly _matDialogRef = inject(MatDialogRef);
+  private readonly _matSnackBar = inject(MatSnackBar);
+
+  readonly transactionStatus = computed(() => this._transactionSender().status);
+  readonly isRunning = computed(() =>
+    this.transactionStatus() === 'sending' ||
+    this.transactionStatus() === 'confirming' ||
+    this.transactionStatus() === 'finalizing',
+  )
+
 
   onTransfer(payload: TransferFormPayload) {
-    console.log('Hola mundo!', payload);
 
-    
+    this._matDialogRef.disableClose = true;
 
     this._transactionSender.send(({publicKey}) => createTransferInstructions({
-      amount: payload.amount,
+      amount: payload.amount * 10**9,
       mintAddress: '7EYnhQoR9YM3N7UoaKRoA44Uy8JeaZV3qyouov87awMs',
       receiverAddress: payload.receiverAddress,
       senderAddress: publicKey.toBase58(),
